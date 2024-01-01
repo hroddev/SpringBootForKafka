7de1071f77622cbebc67d561b364b0896a35f157
Hector Rodriguez <hector@hrod.dev>
2024-01-01 16:40:19 -0500

    Implement the provider and consumer services

--- SpringBootProvider/src/main/java/com/kafka/provider/config/KafkaTopicConfig.java
+++ SpringBootProvider/src/main/java/com/kafka/provider/config/KafkaTopicConfig.java
@@ -2,7 +2,6 @@ package com.kafka.provider.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.config.TopicConfig;
-import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
